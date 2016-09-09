ca577482d2edd0a1362484a666ef88c476c17ec6
Tim Backus <tbackus127@gmail.com>
2016-09-09 10:40:33 -0400

    Created fullscreen borderless window.
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/osuplayer/OsuPlayer.java
+++ src/com/rath/osuplayer/OsuPlayer.java
@@ -1,9 +1,30 @@
+
 package com.rath.osuplayer;
+
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
 
 public class OsuPlayer {
 
-	public static void main(String[] args) {
-		System.err.println("Test");
-	}
+  public static void main(String[] args) {
+
+    // Get screen dimensions
+    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+    JFrame frame = new JFrame();
+
+    frame.setSize(dim);
+    frame.setPreferredSize(dim);
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+    frame.setUndecorated(true);
+    frame.setLayout(null);
+
+    frame.setVisible(true);
+  }
 
 }
