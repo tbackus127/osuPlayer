a59f42b296899be63b5a0627a3f5b36941dbd22d
Tim Backus <tbackus127@gmail.com>
2016-09-12 10:31:38 -0400

    Finds and lists all beatmap directories within 'Songs/'.
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/osuplayer/OsuPlayer.java
+++ src/com/rath/osuplayer/OsuPlayer.java
@@ -4,18 +4,46 @@ package com.rath.osuplayer;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.FilenameFilter;
 
 import javax.swing.JFrame;
 
+/**
+ * @author Tim Backus tbackus127@gmail.com
+ */
 public class OsuPlayer {
 
+  /**
+   * Main method
+   * 
+   * @param args
+   *          Runtime arguments
+   */
   public static void main(String[] args) {
+
+    // Find song directory
+    File songDir = new File("Songs/");
+    System.err.println(songDir.getAbsolutePath());
+    if (!songDir.exists() || !songDir.isDirectory()) {
+      System.err.println("Songs directory not found!");
+    }
+
+    // Get beatmap folder list
+    String[] beatmapFolders = songDir.list(new FilenameFilter() {
+
+      @Override
+      public boolean accept(File curr, String name) {
+        return new File(curr, name).isDirectory();
+      }
+    });
 
     // Get screen dimensions
     Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
     JFrame frame = new JFrame();
 
+    // Create window and display it
     frame.setSize(dim);
     frame.setPreferredSize(dim);
 
