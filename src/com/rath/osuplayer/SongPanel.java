6e2ba33023b292ebbdd70950cd465026d35ad507
Tim Backus <tbackus127@gmail.com>
2016-09-13 11:01:18 -0400

    Start adding javadoc to panel.
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/osuplayer/SongPanel.java
+++ src/com/rath/osuplayer/SongPanel.java
@@ -1,3 +1,4 @@
+
 package com.rath.osuplayer;
 
 import java.awt.Graphics;
@@ -8,21 +9,30 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+/**
+ * The main graphics panel. Song backgrounds and info will be rendered here.
+ * 
+ * @author Administrator
+ * 
+ */
 public class SongPanel extends JPanel {
 
+  /**
+   * The song background from the beatmap folder
+   */
   private Image songBG;
-  
+
   public SongPanel(String[] meta) {
     super();
     try {
       System.err.println(meta[0] + "/" + meta[1]);
-      this.songBG = ImageIO.read(new File(meta[0] + "/" +  meta[1]));
+      this.songBG = ImageIO.read(new File(meta[0] + "/" + meta[1]));
     }
     catch (IOException e) {
       e.printStackTrace();
     }
   }
-  
+
   @Override
   public void paintComponent(Graphics g) {
     g.drawImage(this.songBG, 0, 0, null);
