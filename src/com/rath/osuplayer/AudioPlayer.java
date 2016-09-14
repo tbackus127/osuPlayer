fe7c2674c6ccf4f8d6cb42621bb27485e6d38703
Tim Backus <tbackus127@gmail.com>
2016-09-14 11:50:48 -0400

    Added structure of audio player.
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

---
+++ src/com/rath/osuplayer/AudioPlayer.java
@@ -0,0 +1,25 @@
+package com.rath.osuplayer;
+
+import java.io.File;
+
+
+public class AudioPlayer {
+
+  private boolean isPlaying;
+  
+  public AudioPlayer() {
+    this.isPlaying = false;
+  }
+  
+  public void play() {
+    
+  }
+  
+  public void pause() {
+    
+  }
+  
+  public void changeFile(File f) {
+    
+  }
+}
