73db38ff5b338163ac6ffedbdee998708ce36b41
Tim Backus <tbackus127@gmail.com>
2016-09-16 12:00:01 -0400

    Commented AudioPlayer
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/osuplayer/AudioPlayer.java
+++ src/com/rath/osuplayer/AudioPlayer.java
@@ -1,25 +1,57 @@
+
 package com.rath.osuplayer;
 
 import java.io.File;
 
-
+/**
+ * This class plays audio from an .mp3 file.
+ * 
+ * @author Tim Backus tbackus127@gmail.com
+ */
 public class AudioPlayer {
 
+  /**
+   * Whether or not there is currently audio playing
+   */
   private boolean isPlaying;
-  
+
+  /**
+   * Default constructor
+   */
   public AudioPlayer() {
     this.isPlaying = false;
   }
-  
+
+  /**
+   * Plays the loaded .mp3 file
+   */
   public void play() {
-    
+    isPlaying = true;
   }
-  
+
+  /**
+   * Pauses the currently playing .mp3 file.
+   */
   public void pause() {
-    
+    isPlaying = false;
   }
-  
+
+  /**
+   * Checks whether or not there is currently audio playing
+   * 
+   * @return true if there is audio currently playing; false if not.
+   */
+  public boolean isPlaying() {
+    return isPlaying;
+  }
+
+  /**
+   * Loads audio data from the file specified.
+   * 
+   * @param f
+   *          the .mp3 file to load audio from.
+   */
   public void changeFile(File f) {
-    
+
   }
 }
